42475a4e8a87d601437f2ebc36428ccb2d2c8b04
v_jninzhang <v_jninzhang@tencent.com>
2021-12-10 16:24:49 +0800

    notes(vue): 组件设计之插槽slot

---
+++ 01组件设计/solt/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Father/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Father from './components/Father.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Father
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 01组件设计/solt/src/components/Father.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <s-index-layout>
+            <template v-slot:header>
+                hahah
+            </template>
+            <template v-slot:default>
+                xixii
+            </template>
+            <template v-slot:footer>
+                lilili
+            </template>
+        </s-index-layout>
+    </div>
+</template>
+<script>
+import SIndexLayout from './SIndexLayout.vue'
+
+export default {
+  components: {
+    SIndexLayout
+  }
+}
+</script>
